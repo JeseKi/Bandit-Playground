95c86155c4b4c62d5d0ec9173eb972646618d617
Jese Ki <2094901072@qq.com>
2025-09-29 13:00:54 +0800

    FEAT: Complete Base Agent ABC

---
+++ bandit_lib/agents/base.py
@@ -0,0 +1,149 @@
+from __future__ import annotations
+
+from typing import List, TypeVar, Generic, Type
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from .schemas import AlgorithmConfig, BaseRewardStates, Metrics, MetricsConfig
+from bandit_lib.env import Environment
+from bandit_lib.utils import ProcessDataLogger
+
+AgentReward_T = TypeVar("AgentReward_T", bound=BaseRewardStates)
+AgentAlgorithm_T = TypeVar("AgentAlgorithm_T", bound="BaseAlgorithm")
+Agent_T = TypeVar("Agent_T", bound="BaseAgent")
+
+
+class BaseAlgorithm(ABC, Generic[Agent_T]):
+    def __init__(self, config: AlgorithmConfig, agent_type: Type[Agent_T]) -> None:
+        self._agent: Agent_T
+        self._target_type: Type[Agent_T] = agent_type
+        self.config: AlgorithmConfig = config
+
+    @property
+    def agent(self) -> Agent_T:
+        return self._agent
+
+    @agent.setter
+    def agent(self, agent: Agent_T) -> None:
+        if not isinstance(agent, self._target_type):
+            raise ValueError(f"Agent must be of type {self._target_type}")
+        self._agent = agent
+
+    @abstractmethod
+    def run(self) -> int:
+        raise NotImplementedError
+
+
+class BaseAgent(ABC, Generic[AgentReward_T, AgentAlgorithm_T]):
+    def __init__(
+        self,
+        name: str,
+        env: Environment,
+        algorithm: AgentAlgorithm_T,
+        metrics_config: MetricsConfig = MetricsConfig(),
+        process_data_logger: ProcessDataLogger | None = None,
+        seed: int = 42,
+    ) -> None:
+        "self.rewards_states must be set in the subclass. Such as `self.rewards_states = BaseRewardStates.create(self.env.arm_num)`."
+        # config
+        self.name: str = name
+        self.env: Environment = env
+        self.algorithm: AgentAlgorithm_T = algorithm
+        self.algorithm.agent = self
+        self.seed: int = seed
+        self.rng: np.random.Generator = np.random.default_rng(seed)
+        self.metrics_config: MetricsConfig = metrics_config
+        self.process_data_logger: ProcessDataLogger | None = process_data_logger
+        
+        # state
+        self.steps: int = 0
+        self.optimal_arm_chosen_times: int = 0
+        self.metrics: List[Metrics] = []
+        self.convergence_step: int = -1
+        
+        self._convergenced: bool = False
+        
+        # init
+        self.rewards_states: AgentReward_T # must be set in the subclass
+        if self.process_data_logger is not None:
+            self.process_data_logger.agent = self
+    
+    def step(self) -> None:
+        """Step the agent."""
+        arm_index = self.act()
+        reward = self.pull(arm_index=arm_index)
+        self.update(arm_index=arm_index, reward=reward)
+
+    @abstractmethod
+    def act(self) -> int:
+        """Action. Return the arm index to pull. Return a arm index."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def pull(self, arm_index: int) -> int:
+        """Pull the arm. Unlike `act`, this function will return the reward."""
+        raise NotImplementedError
+
+    def update(self, arm_index: int, reward: int) -> None:
+        self.steps += 1
+        # update rewards
+        self.rewards_states.rewards[arm_index, 0] += 1
+        self.rewards_states.rewards[arm_index, 1] += reward
+        self.rewards_states.sliding_window_rewards.append((1, reward))
+        
+        # update metrics
+        if arm_index == self.env.best_arm_index:
+            self.optimal_arm_chosen_times += 1
+            
+        if not self.env.config.enable_dynamic and \
+            not self._convergenced and \
+                self.optimal_arm_rate() >= self.metrics_config.optimal_arm_rate_threshold:
+            self._convergenced = True
+            self.convergence_step = self.steps
+        
+        if self.process_data_logger is not None and self.process_data_logger.should_record(self.steps):
+            metrics = Metrics(
+                current_step=self.steps,
+                regret_rate=self.regret_rate(),
+                regret=self.regret(),
+                reward_rate=self.reward_rate(),
+                reward=self.total_reward(),
+                optimal_arm_rate=self.optimal_arm_rate(),
+                sliding_window_reward_rate=self.sliding_window_reward_rate(),
+                convergence_step=self.convergence_step,
+            )
+            self.process_data_logger.record(metrics)
+
+    def regret(self) -> float:
+        """Calculate the regret of the agent."""
+        best_reward = self.env.best_arm.reward_probability * self.steps
+        return best_reward - self.total_reward()
+
+    def regret_rate(self) -> float:
+        """Calculate the regret rate of the agent."""
+        if self.steps == 0:
+            return 0
+        best_reward = self.env.best_arm.reward_probability * self.steps
+        return self.regret() / best_reward
+
+    def reward_rate(self) -> float:
+        """Calculate the reward rate of the agent."""
+        if self.steps == 0:
+            return 0
+        return self.total_reward() / self.steps
+    
+    def total_reward(self) -> int:
+        """Calculate the total reward of the agent."""
+        r = int(np.sum(self.rewards_states.rewards[:, 1]))
+        return r
+    
+    def optimal_arm_rate(self) -> float:
+        """Calculate the optimal arm rate of the agent."""
+        return self.optimal_arm_chosen_times / self.steps
+    
+    def sliding_window_reward_rate(self) -> float:
+        """Calculate the sliding window reward rate of the agent."""
+        rewards: int = np.sum([reward for _, reward in self.rewards_states.sliding_window_rewards])
+        pull_counts: int = np.sum([pull_count for pull_count, _ in self.rewards_states.sliding_window_rewards])
+        return rewards / pull_counts
---
+++ bandit_lib/agents/schemas.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from enum import Enum
+from collections import deque
+from typing import Deque, Tuple
+
+import numpy as np
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class BaseRewardStates(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    rewards: np.ndarray = Field(
+        ...,
+        description="Rewards of the agent, shape: (num_arms, 2) where stats[i, 0] = arm i's pull count and stats[i, 1] = arm i's reward value.",
+    )
+    sliding_window_rewards: Deque[Tuple[float, float]] = Field(
+        ...,
+        description="Rewards of the agent in a sliding window. Tuple of (pull count, reward value).",
+    )
+    
+    @classmethod
+    def create(cls, arm_num: int, sliding_window_size: int) -> "BaseRewardStates":
+        return cls(rewards=np.zeros((arm_num, 2)), sliding_window_rewards=deque(maxlen=sliding_window_size))
+
+class AlgorithmType(Enum):
+    GREEDY = "greedy"
+    UCB1 = "ucb1"
+    THOMPSON_SAMPLING = "thompson_sampling"
+
+class AlgorithmConfig(BaseModel):
+    algorithm_type: AlgorithmType
+    optimistic_initialization_enabled: bool = Field(default=False)
+    optimistic_initialization_value: float = Field(default=1)
+    enable_decay_alpha: bool = Field(default=False)
+    constant_step_decay_alpha: float = Field(default=0.1)
+    discount_factor: float = Field(default=0.9)
+    
+
+class Metrics(BaseModel):
+    """Metrics of the agent.
+    In order to use the class to collect metrics while not training, some fields are initialized with -1.
+    """
+    current_step: int = Field(default=-1, description="The step number of the metrics when it is collected.")
+    regret_rate: float
+    regret: float
+    reward_rate: float
+    reward: int
+    sliding_window_reward_rate: float = Field(default=-1, description="A reward rate that is in any sliding window.")
+    optimal_arm_rate: float
+    convergence_step: int = Field(
+        default=-1,
+        description="Using in static environment. Step count when the agent converges to the optimal arm.",
+    )
+
+class MetricsConfig(BaseModel):
+    metrics_history_size: int = Field(default=500)
+    sliding_window_size: int = Field(default=1000)
+    optimal_arm_rate_threshold: float = Field(default=0.95)
+    min_convergence_step: int = Field(default=100, description="The minimum step count when the agent converges to the optimal arm.")
